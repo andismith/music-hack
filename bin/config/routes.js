ff2cae5707c51ad672a1a9f1acc2261e5d0ea5fd
Tom Maton <letstalk@tom-maton.co.uk>
2013-07-20 19:45:39 +0100

    randomised id

--- bin/config/routes.js
+++ bin/config/routes.js
@@ -4,6 +4,7 @@
 (function () {
     "use strict";
 	var nokiaAPI = require('../nokia-api/stream'),
+		config = require('../config/app_config'),
 		songsSearchResults;
 
 		new nokiaAPI.NokiaMusic().getRandomSong(songsRetrieved);
@@ -13,12 +14,44 @@
 		}
 
 		module.exports = function(app) {
+			var resultCount;
+
 			app.get('/', function (req, res){ 
-				res.render('index', JSON.parse(songsSearchResults));			
+
+
+				var parsedResults = JSON.parse(songsSearchResults),
+					randomId = pickRandomId(parsedResults.tracklist);
+					
+					resultCount = parsedResults.tracklist.length;
+					//randomList(parsedResults.tracklist);
+
+				res.render('index', {'selected': randomId, 'results': randomList(parsedResults.tracklist)});			
 			});
             app.get('/web-audio-test', function (req, res){
                 res.header('Access-Control-Allow-Origin', '*');
                 res.render('web-audio-test', res);           
             });
+
+            function pickRandomId(results) {
+            	//console.log(results)
+            	var randomNumber = Math.floor((Math.random()*+config.AppConfig.Results.count));
+
+            	return results[randomNumber].id;
+	        }
+
+	        function randomList(results) {
+	        	var arr = [];
+	        		arr.length =0;
+				while(arr.length < config.AppConfig.Results.count){
+				  var randomnumber=Math.floor(Math.random()+(resultCount-1));
+				  var found=false;
+				  for(var i=0;i<arr.length;i++){
+				    if(arr[i]==randomnumber){found=true;break}
+				  }
+				  if(!found)arr[arr.length]=results[i].name;
+				}
+				return arr;
+
+	        }	     		
 		};
 }());
