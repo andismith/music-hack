ee07f181b7eae15ce34768c2d64a4e4b434b5177
C J Marsh <public@cjmarsh.com>
2013-07-21 11:02:17 +0100

    Add unit tests.

--- bin/data_access/redis.js
+++ bin/data_access/redis.js
@@ -4,19 +4,39 @@ module.exports = (function(){
 		util = require('./dbutil');
 
 	function addScore(username, score, callback) {
-		var client = redis.createClient(config.AppConfig.Redis.port, config.AppConfig.Redis.host);
-		client.ZADD(config.AppConfig.Redis.leaderboard, score, username, function(err) {
-			util.handleError(err);
-			client.quit();
-			util.safeCallback(function() {
-				callback();
-			});
+		findScore(username, function(currentScore) {
+			if (!currentScore || currentScore < score) {
+				var client = redis.createClient(config.AppConfig.Redis.port, config.AppConfig.Redis.host);
+				client.ZADD(config.AppConfig.Redis.leaderboard, score, username, function(err) {
+					util.handleError(err);
+					client.quit();
+					util.safeCallback(function() {
+						callback();
+					});
+				});
+			}
+			else {
+				util.safeCallback(function() {
+					callback(score);
+				});
+			}
 		});
 	}
-	
+
 	function findScore(username, callback) {
 		var client = redis.createClient(config.AppConfig.Redis.port, config.AppConfig.Redis.host);
 		client.ZSCORE(config.AppConfig.Redis.leaderboard, username, function(err, score) {
+			util.handleError(err);
+			client.quit();
+			util.safeCallback(function() {
+				callback(score);
+			});
+		});
+	}
+
+	function leaderboard(size) {
+		var client = redis.createClient(config.AppConfig.Redis.port, config.AppConfig.Redis.host);
+		client.ZREVRANGE(config.AppConfig.Redis.leaderboard, 0, size-1, 'WITHSCORE', function(err, lb) {
 			util.handleError(err);
 			client.quit();
 			util.safeCallback(function() {
--- tests/data-access/redis-spec.js
+++ tests/data-access/redis-spec.js
@@ -30,7 +30,7 @@ describe("Writing to Redis", function(){
 	    });
 	});
 
-	it("should add increase score for existing user is score is higher", function() {
+	it("should change score for existing user if new score is higher", function() {
 		// Given
 		var now = new Date(),
 			username = "un_"+now,
@@ -50,14 +50,73 @@ describe("Writing to Redis", function(){
 				});
 			});
 		});
-		
+
 		waitsFor(function(){
 			return flag;
 		}, 'the correct records should be returned', 50);
-		
+
 		// Then
 		runs(function() {
 			expect(highScore).toMatch(secondScoreToAdd);
 	    });
+	});
+
+	it("should not change score for existing user if new score is lower", function() {
+		// Given
+		var now = new Date(),
+			username = "un_"+now,
+			firstScoreToAdd = 123459,
+			secondScoreToAdd = 123458,
+			highScore;
+
+		// When
+		runs(function() {
+			flag = false;
+			redis.addScore(username, firstScoreToAdd, function() {
+				redis.addScore(username, secondScoreToAdd, function() {
+					highScore = redis.findScore(username, function(score) {
+						highScore = score;
+						flag = true;
+					});
+				});
+			});
+		});
+
+		waitsFor(function(){
+			return flag;
+		}, 'the correct records should be returned', 50);
+
+		// Then
+		runs(function() {
+			expect(highScore).toMatch(firstScoreToAdd);
+	    });
+	});
+
+	it("should return top x records in leaderboard", function() {
+		// Given
+		var scores = {"user1":10, "user2":9, "user3":8, "user4":7, "user5":6, "user6":5};
+/*
+		// When
+		runs(function() {
+			flag = false;
+			redis.addScore(username, firstScoreToAdd, function() {
+				redis.addScore(username, secondScoreToAdd, function() {
+					highScore = redis.findScore(username, function(score) {
+						highScore = score;
+						flag = true;
+					});
+				});
+			});
+		});
+
+		waitsFor(function(){
+			return flag;
+		}, 'the correct records should be returned', 50);
+
+		// Then
+		runs(function() {
+			expect(highScore).toMatch(firstScoreToAdd);
+	    });
+		*/
 	});
 });
